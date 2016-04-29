69f4dd2c47c3090c81a71d2e76e8a065b29ba1ea
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-04-29 18:17:10 -0500

    First Euler problem solved!

---
+++ 001.py
@@ -0,0 +1,16 @@
+# Completed on 4/29/2016 by Theresa Thoraldson
+# source: https://projecteuler.net/problem=1
+
+import math
+
+dict = []
+
+print("Euler 001")
+
+for x in range(0, 1000):
+  if (x % 5) == 0:
+    dict.append(x)
+  if (x % 3) == 0:
+    dict.append(x)
+    
+print list(set(dict))
