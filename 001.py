469e15d60d6dc1d84631e784d31d0528379a5f10
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-04-29 19:59:13 -0500

    updated 001.py
    actually made a print function to show the sum of the factorials!

--- 001.py
+++ 001.py
@@ -1,6 +1,5 @@
 # Completed on 4/29/2016 by Theresa Thoraldson
 # source: https://projecteuler.net/problem=1
-
 import math
 
 dict = []
@@ -12,5 +11,9 @@ for x in range(0, 1000):
     dict.append(x)
   if (x % 3) == 0:
     dict.append(x)
-    
+
+print("list of factorials:"),
 print list(set(dict))
+print("")
+print("Sum of factorials:"),
+print sum(list(set(dict)))
