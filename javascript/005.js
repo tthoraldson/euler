aa0a9605e9831dda00097bf43ee652f750a9babd
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-10-24 13:04:40 -0500

    super slow 005.js

---
+++ javascript/005.js
@@ -0,0 +1,17 @@
+/* Completed on 10/24/2016 by Theresa Thoraldson
+   source: https://projecteuler.net/problem=5 */
+
+// var i = 1; // current number
+// var x = 1; // current divider
+//
+// do {
+//     console.log(i + ' % ' + x + ' === ' + i % x);
+//     if (i % x === 0){
+//       x++;
+//     } else {
+//       i++;
+//       x = 1;
+//     }
+// } while (x <= 20);
+//
+// console.log(i); // RETURNS 232792560 !!
